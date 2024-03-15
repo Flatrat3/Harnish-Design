c0ee2834f3ff073747653e4594264dc057027fb9
Asim Ehmed <rocco.borocco95@gmail.com>
2024-03-15 23:20:08 +0400

    added slider for images

--- app.js
+++ app.js
@@ -93,3 +93,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+var swiper = new Swiper(".mySwiper", {
+
+
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    mousewheel: true,
+    keyboard: true,
+});
+
+swiper.setGrabCursor()
